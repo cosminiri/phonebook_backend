1cbe1756641832424e7f556e1ab6b2fb405fbeb4
cosminiri <cosmin.irimescu69@gmail.com>
2025-08-19 17:38:28 +0300

    removed cors

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express')
-const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
 app.use(express.static('dist'))
-app.use(cors())
 app.use(express.json())
 
 morgan.token('body', (request, response) => JSON.stringify(request.body))
